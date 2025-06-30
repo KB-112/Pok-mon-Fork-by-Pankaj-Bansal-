984be7765057e93e378b36eae0ec0693fc35f977
KB <79893099+KB-112@users.noreply.github.com>
2025-06-30 17:02:49 +0530

    Input_Name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+using namespace std;
 
-    int main() {
+int main() {
 
-      
-      
-      
-        return 0;
-    }
+    string playerName;
+
+
+    cout << "Trainer! Before you embark on your journey, the Pokemon world needs to know who you are!\n";
+    cout<<"Time to step up and tell us your name And remember, a great name is the start of every great adventure!\n";      
+   
+    cout << "\n" << endl;
+    cout << "Player Name : ";
+    cin >> playerName ;
+    cout << "\n" <<endl;
+   
+    cout << "Great start," << playerName << "! Looks like you’ve mastered the opening of every code adventure.Keep this up, and you’ll be a coding master in no time!" << endl;
+
+
+    return 0;
+}
 
